25487c6b5d67658ba96cf70dfd914b55eff39737
wfarid <walidfarid.wf@gmail.com>
2014-09-17 22:05:13 -0600

    updating blog.py to check the credentials (username and password) input from a html form in login.html

--- blog.py
+++ blog.py
@@ -6,20 +6,39 @@ from flask import Flask, render_template, request, session, \
                   flash, redirect, url_for, g
 
 # Configuration
-DATABASE = "blog.db"
+DATABASE = 'blog.db'
+USERNAME = 'admin'
+PASSWORD = 'admin'
+SECRET_KEY = 'hard_to_guess'    #-- TODO: This key need to be changed before deployment
 
 app = Flask(__name__)
 
 # Pulling in the app configuration by looking for UPPERCASE variables
 app.config.from_object(__name__)
 
-@app.route('/')
+@app.route('/',methods = ['GET','POST'])
 def login():
-    return render_template("login.html")
+    error = None
+
+    if request.method == 'POST':
+        if request.form['username'] != app.config["USERNAME"] or \
+           request.form['password'] != app.config["PASSWORD"]:
+            error = "Username and/or Password don't match"
+        else:
+            session['logged_in'] = True
+            return redirect(url_for('main'))
+
+    return render_template("login.html", error = error)
 
 @app.route('/main')
 def main():
     return render_template("main.html")
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in',None)
+    flash('You were logged out')
+    return redirect(url_for('login'))
 
 def get_db_connection():
     return sqlite3.connect(app.config["DATABASE"])
